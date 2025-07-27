2593699f61423ccb8611e424feebd6c81d094669
_Bhavesh.malviya <94284112+Op998200@users.noreply.github.com>
2025-07-27 22:34:26 +0530

    Delete Cashivo-Web/Cashivo directory

--- Cashivo-Web/Cashivo/script/supabase.js
+++
@@ -1,7 +0,0 @@
-// script/supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const supabaseUrl = 'https://fjwodxbrnsutnvsooduh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqd29keGJybnN1dG52c29vZHVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MTk4MjAsImV4cCI6MjA2OTE5NTgyMH0.GePnmFWipZxwU6UVkXSnPWRV3EX-BJePB5IdCtElOQg';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
